9fc3852bae85c5e816ffdd46627c3e953587c4f7
Olga_Basheva <olhabasheva@yandex.ru>
2026-01-18 01:31:36 +0800

    front config file changed to allow connection via a manually created temporaty tunnel

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -20,6 +20,8 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    host: '0.0.0.0',
+    allowedHosts: ['.loca.lt', 'localhost'],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
